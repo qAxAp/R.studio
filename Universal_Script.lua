f1bf6c847255454fa5fd26e653abe63e8f42e756
AxA <162728663+qAxAp@users.noreply.github.com>
2025-04-03 18:07:39 +0200

    Update and rename Universal Script to Universal_Script.lua

---
+++ Universal_Script.lua
@@ -0,0 +1,96 @@
+local player = game.Players.LocalPlayer
+local playerGui = player:WaitForChild("PlayerGui")
+
+local screenGui = Instance.new("ScreenGui")
+screenGui.Parent = playerGui
+
+local frame1 = Instance.new("Frame")
+frame1.Name = "Frame1"
+frame1.Size = UDim2.new(0.4666666666666667, 0, 0.3342981186685962, 0)
+frame1.Position = UDim2.new(0.171875, 0, 0.17800289435600578, 0)
+frame1.BackgroundColor3 = Color3.new(0.110, 0.110, 0.110)
+frame1.Parent = screenGui
+
+local frame2 = Instance.new("Frame")
+frame2.Name = "Frame2"
+frame2.Size = UDim2.new(0.13854166666666667, 0, 0.2952243125904486, 0)
+frame2.Position = UDim2.new(0.184375, 0, 0.19681620839363242, 0)
+frame2.BackgroundColor3 = Color3.new(0.180, 0.180, 0.180)
+frame2.Parent = screenGui
+
+local frame3 = Instance.new("Frame")
+frame3.Name = "Frame3"
+frame3.Size = UDim2.new(0.29583333333333334, 0, 0.2952243125904486, 0)
+frame3.Position = UDim2.new(0.32916666666666666, 0, 0.19681620839363242, 0)
+frame3.BackgroundColor3 = Color3.new(0.180, 0.180, 0.180)
+frame3.Parent = screenGui
+
+local button4 = Instance.new("TextButton")
+button4.Name = "TextButton4"
+button4.Size = UDim2.new(0.11041666666666666, 0, 0.049204052098408106, 0)
+button4.Position = UDim2.new(0.19895833333333332, 0, 0.21273516642547033, 0)
+button4.BackgroundColor3 = Color3.new(0.400, 0.400, 0.400)
+button4.Parent = screenGui
+button4.Text = "Click Me"
+button4.TextColor3 = Color3.new(0.400, 0.400, 0.400)
+
+local button5 = Instance.new("TextButton")
+button5.Name = "TextButton5"
+button5.Size = UDim2.new(0.11041666666666666, 0, 0.05065123010130246, 0)
+button5.Position = UDim2.new(0.19895833333333332, 0, 0.2778581765557164, 0)
+button5.BackgroundColor3 = Color3.new(0.400, 0.400, 0.400)
+button5.Parent = screenGui
+button5.Text = "Click Me"
+button5.TextColor3 = Color3.new(0.400, 0.400, 0.400)
+
+local button6 = Instance.new("TextButton")
+button6.Name = "TextButton6"
+button6.Size = UDim2.new(0.109375, 0, 0.05065123010130246, 0)
+button6.Position = UDim2.new(0.2, 0, 0.34442836468885674, 0)
+button6.BackgroundColor3 = Color3.new(0.439, 0.439, 0.439)
+button6.Parent = screenGui
+button6.Text = "Click Me"
+button6.TextColor3 = Color3.new(0.439, 0.439, 0.439)
+
+local button7 = Instance.new("TextButton")
+button7.Name = "TextButton7"
+button7.Size = UDim2.new(0.10833333333333334, 0, 0.05209840810419682, 0)
+button7.Position = UDim2.new(0.20104166666666667, 0, 0.4109985528219971, 0)
+button7.BackgroundColor3 = Color3.new(0.439, 0.439, 0.439)
+button7.Parent = screenGui
+button7.Text = "Click Me"
+button7.TextColor3 = Color3.new(0.439, 0.439, 0.439)
+
+local frame8 = Instance.new("Frame")
+frame8.Name = "Frame8"
+frame8.Size = UDim2.new(0.46979166666666666, 0, 0.04630969609261939, 0)
+frame8.Position = UDim2.new(0.17083333333333334, 0, 0.13748191027496381, 0)
+frame8.BackgroundColor3 = Color3.new(0.188, 0.188, 0.188)
+frame8.Parent = screenGui
+
+local button9 = Instance.new("TextButton")
+button9.Name = "TextButton9"
+button9.Size = UDim2.new(0.07291666666666667, 0, 0.053545586107091175, 0)
+button9.Position = UDim2.new(0.34479166666666666, 0, 0.21852387843704776, 0)
+button9.BackgroundColor3 = Color3.new(0.439, 0.439, 0.439)
+button9.Parent = screenGui
+button9.Text = "Click Me"
+button9.TextColor3 = Color3.new(0.439, 0.439, 0.439)
+
+local button10 = Instance.new("TextButton")
+button10.Name = "TextButton10"
+button10.Size = UDim2.new(0.071875, 0, 0.053545586107091175, 0)
+button10.Position = UDim2.new(0.44166666666666665, 0, 0.2170767004341534, 0)
+button10.BackgroundColor3 = Color3.new(0.400, 0.400, 0.400)
+button10.Parent = screenGui
+button10.Text = "Click Me"
+button10.TextColor3 = Color3.new(0.400, 0.400, 0.400)
+
+local button11 = Instance.new("TextButton")
+button11.Name = "TextButton11"
+button11.Size = UDim2.new(0.071875, 0, 0.053545586107091175, 0)
+button11.Position = UDim2.new(0.5354166666666667, 0, 0.21852387843704776, 0)
+button11.BackgroundColor3 = Color3.new(0.400, 0.400, 0.400)
+button11.Parent = screenGui
+button11.Text = "Click Me"
+button11.TextColor3 = Color3.new(0.400, 0.400, 0.400)
