9cfa465c982ed7aa83d7ab63872589699231521e
AxA <162728663+qAxAp@users.noreply.github.com>
2025-04-05 17:55:10 +0200

    Update Universal_Script.lua

--- Universal_Script.lua
+++ Universal_Script.lua
@@ -1,5 +1,5 @@
 -- Example usage matching your image:
-local Library = loadstring(game:HttpGet('https://raw.githubusercontent.com/qAxAp/R.studio/refs/heads/main/loadstring?token=GHSAT0AAAAAADBWCFHMK4BUNJDMVMQRUCHUZ7RKDHA'))()
+local Library = loadstring(game:HttpGet('https://raw.githubusercontent.com/qAxAp/R.studio/refs/heads/main/loadstring'))()
 
 local Window = Library:CreateWindow("DioHub", "V6", 10044538000)
 
